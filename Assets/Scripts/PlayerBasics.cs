6c7664ff28637ae18755d7684d596060f736400b
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-01-05 23:34:11 +0100

    creating scene

---
+++ Assets/Scripts/PlayerBasics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBasics : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        //Movement
+        
+    }
+}
