481dfca54e4154aa4e6bb7c3c8adb51c9f7ccff6
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-01-06 23:27:20 +0100

    player basics + making scene

--- Assets/Scripts/PlayerBasics.cs
+++ Assets/Scripts/PlayerBasics.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlayerBasics : MonoBehaviour
 {
     Rigidbody2D rb;
+    float speed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        speed = 100;
     }
 
     void Update()
     {
         //Movement
-        
+        float hInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(hInput * speed *Time.deltaTime * Time.deltaTime, 0, 0);
     }
 }
