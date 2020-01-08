d6b446dda5b815263093ccc77ebeaf36bfdbe6b4
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-01-08 23:37:17 +0100

    trying to do a fest but didnt work

--- Assets/Scripts/PlayerBasics.cs
+++ Assets/Scripts/PlayerBasics.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerBasics : MonoBehaviour
 {
+    //stairs
+    public Transform stairdetect;
+    public int lolaso;
+
     Rigidbody2D rb;
     float speed;
 
@@ -13,10 +17,20 @@ public class PlayerBasics : MonoBehaviour
         speed = 100;
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        //Debug.DrawRay(transform.position, transform.right, Color.blue);
+        //if (/*RaycastHit2D hit = */Physics2D.Raycast(transform.position, new Vector2(lolaso, 0)).collider)
+        //{
+        //    print("hit");
+        //}
+
         //Movement
         float hInput = Input.GetAxis("Horizontal");
         transform.position += new Vector3(hInput * speed *Time.deltaTime * Time.deltaTime, 0, 0);
+
+        //Animation(stairs)
+        
+        
     }
 }
