71bcfa51e837c51e79eb98c024a2c3ac65dba213
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-05-14 02:00:36 +0200

    gain health

---
+++ Tech-School-Project/Assets/Scripts/GainHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GainHealth : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position.y = Random.Range(-6f, 7f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
