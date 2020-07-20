893ec9684652e677b45f35c8d12a33a2ada1909f
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-07-21 01:39:39 +0200

    reverting

---
+++ Tech-school-project-mobile/Assets/Scripts/SetHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SetHealth : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float healthset;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        healthset = 0;
+    }
+    void Update()
+    {
+        if(healthset <= -rb.mass)
+        {
+            healthset = -rb.mass;
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag != "Wall")
+        {
+            healthset -= 0.5f;
+        }
+    }
+}
---
+++ Tech-school-project-mobile/Assets/Scripts/testing/MovementForX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementForX : MonoBehaviour
+{
+    public GameObject circle;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition);
+
+        if(hit)
+        {
+            Debug.DrawLine(Input.mousePosition, transform.position, Color.green);
+            Instantiate(circle, hit.point, transform.rotation);
+        }
+        else
+        {
+            Debug.DrawLine(Input.mousePosition, transform.position, Color.red);
+        }
+    }
+}
