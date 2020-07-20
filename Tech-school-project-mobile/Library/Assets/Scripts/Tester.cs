277ac35bec362eff77c4112e4bc30b28e2420f1f
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-07-21 01:50:18 +0200

    revert pngs

---
+++ Tech-school-project-mobile/Library/Assets/Scripts/Tester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tester : MonoBehaviour
+{
+    BoxCollider2D lav;
+
+    void Awake()
+    {
+        lav = gameObject.GetComponent<BoxCollider2D>();
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            Destroy(GameObject.FindWithTag("player"));
+        }
+    }
+}
