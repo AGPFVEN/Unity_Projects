20e7dd9ab4f731e478b517eefe8b7293a5e1b1ec
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-08-02 14:37:39 -1000

    testing

--- Tech-School-Project/Assets/Scripts/testing/test1.cs
+++ Tech-School-Project/Assets/Scripts/testing/test1.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class test1 : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    IEnumerator Uploadscore()
     {
-        
+        WWWForm form = new WWWForm();
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/Backend-repository/AGPFVEN/Tech-School-Project_php/save_mark.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
     }
 }
