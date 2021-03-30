62f673f9213cace2631068dac2a030a81ddc5620
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-03-30 03:46:53 +0200

    let's create a new terrain algorithm

---
+++ Tech-School-Project/Assets/Scripts/CameraAlternation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAlternation : MonoBehaviour
+{
+    //Lava position
+    Transform lava;
+
+    void Start()
+    {
+        //Lava position
+        lava = GameObject.Find("Lava (1)").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //Jump ray
+        // RaycastHit2D fireRay = Physics2D.Raycast(cannon_Transform_Top.position, lookDirection.normalized);
+        Debug.DrawRay(lava.position, new Vector3(1,0,0), Color.green);
+    }
+}
